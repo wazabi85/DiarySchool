f792194fed5e2251706567ba91ac9a761ff955fc
wazabi85 <wazabi@mail.ru>
2017-01-05 23:06:23 +0500

    navigation bar

--- app/src/main/java/com/example/wazabi/diaryschool/MainActivity.java
+++ app/src/main/java/com/example/wazabi/diaryschool/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.wazabi.diaryschool;
 
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -7,15 +8,21 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int LAYOUT = R.layout.activity_main;
     private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(LAYOUT);
+
         initToolbar();
+        intiNavigationView();
     }
+
+
 
     private void initToolbar() {
         toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -26,6 +33,13 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             }
         });
+
         toolbar.inflateMenu(R.menu.menu);
+    }
+
+
+
+    private void intiNavigationView() {
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
     }
 }
