de6b7640ce2f82fc1617aa71f9e35050f82a6389
wazabi85 <wazabi@mail.ru>
2017-01-05 22:01:09 +0500

    navigation bar

--- app/src/main/java/com/example/wazabi/diaryschool/MainActivity.java
+++ app/src/main/java/com/example/wazabi/diaryschool/MainActivity.java
@@ -1,20 +1,19 @@
 package com.example.wazabi.diaryschool;
 
-import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         initToolbar();
     }
 
